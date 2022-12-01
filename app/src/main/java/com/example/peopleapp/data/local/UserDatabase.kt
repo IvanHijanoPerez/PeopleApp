4b7f36cb9153d44ab02353dc5271c2981e34fc54
IvanHijanoPerez <62576321+IvanHijanoPerez@users.noreply.github.com>
2022-12-01 01:25:20 +0100

    formated code and uploaded screenshots for readme

--- app/src/main/java/com/example/peopleapp/data/local/UserDatabase.kt
+++ app/src/main/java/com/example/peopleapp/data/local/UserDatabase.kt
@@ -10,6 +10,6 @@ import com.example.peopleapp.domain.model.User
     version = 1,
     exportSchema = false
 )
-abstract class UserDatabase: RoomDatabase() {
-    abstract  val userDao: UserDao
+abstract class UserDatabase : RoomDatabase() {
+    abstract val userDao: UserDao
 }
--- app/src/main/java/com/example/peopleapp/presentation/user_edit/UserEditEvent.kt
+++ app/src/main/java/com/example/peopleapp/presentation/user_edit/UserEditEvent.kt
@@ -1,8 +1,8 @@
 package com.example.peopleapp.presentation.user_edit
 
 sealed class UserEditEvent {
-    data class EnteredName(val name: String): UserEditEvent()
-    data class EnteredLastName(val lastName: String): UserEditEvent()
-    data class EnteredAge(val age: String): UserEditEvent()
-    object InsertUser: UserEditEvent()
+    data class EnteredName(val name: String) : UserEditEvent()
+    data class EnteredLastName(val lastName: String) : UserEditEvent()
+    data class EnteredAge(val age: String) : UserEditEvent()
+    object InsertUser : UserEditEvent()
 }
